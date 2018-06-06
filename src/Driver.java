5fab84225071e698c64c37402f7216a3b1ab1b87
user <user@192.168.10.103>
2018-06-06 13:06:27 -0400

    One input added

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 
 public class Driver {
 
 	public static void main(String[] args) {
+		String name;
 		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("What is your name");
+		name = input.next();
+		System.out.printf("My name is: %s", name );
 	}
 
 }
